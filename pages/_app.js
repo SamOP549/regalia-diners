c46d8e32b0979083c12e00ac525ffe4d92712840
SamOP549 <jainsamarth549@gmail.com>
2023-01-08 16:22:27 +0530

    analytics removed

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import StickyIcon from '../components/StickyIcon'
-import { Analytics } from '@vercel/analytics/react';
 import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
@@ -13,7 +12,6 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
         <StickyIcon />
         <Footer pathname={router.pathname} />
-        <Analytics />
       </div>
   )
 
