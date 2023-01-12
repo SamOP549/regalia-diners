c1f15adfd242c6e66cb5c9fd785edc2beef10562
SamOP549 <jainsamarth549@gmail.com>
2023-01-13 02:47:11 +0530

    error fixed

--- pages/services/index.js
+++ pages/services/index.js
@@ -16,30 +16,28 @@ const Services = () => {
       imageAlt: "Catering Service",
       title: "Catering",
       btnHref: '/services/catering',
-      desc: "Catering is the most important aspect of any event. Your guests, colleagues, contemporaries, friends or executives will have a lasting impression of your event if you satisfy their taste buds. Nobody remembers what songs DJ was playing or which flower were used to welcome them what they do remember is the Food. Catering is something you don't want to take chance with."
+      desc: `<p>Catering is the most important aspect of any event. Your guests, colleagues, contemporaries, friends or executives will have a lasting impression of your event if you satisfy their taste buds. Nobody remembers what songs DJ was playing or which flower were used to welcome them what they do remember is the Food. Catering is something you don't want to take chance with.</p>`
     },
     {
       imageSrc: decor,
       imageAlt: "Decoration Service",
       title: "Decoration",
       btnHref: '/services/decoration',
-      desc: "Its your day, so you should be the last person to make any adjustments to your design. We provide specific decorations as per the need of our clients. Most of our brides and grooms already have in their mind what they want their day to look like, and we are here to provide it with the designs similar to the most minute details."
+      desc: `<p>Its your day, so you should be the last person to make any adjustments to your design. We provide specific decorations as per the need of our clients. Most of our brides and grooms already have in their mind what they want their day to look like, and we are here to provide it with the designs similar to the most minute details.</p>`
     },
     {
       imageSrc: makeup,
       imageAlt: "Makeup Service",
       title: "Makeup",
       btnHref: '/services/makeup',
-      desc: "We believe in perfection, makeup is not a layer to hide one's imperfections but to highlight one's features. Your big day is important to us.",
-      desc2: "We offer you with best makeup services, transforming your look and making sure that you looking stunning on your big day and throughout the wedding ceremonies. Artist here understands your requirements and provides you with the makeup that goes along well with your look, making sure that you slay  the day!",
-      desc3: "Services provided - Our services include makeup on venue for the bride and guests,  hairstyling, draping, men's grooming, nail extensions for all occasions."
+      desc: `<p>We believe in perfection, makeup is not a layer to hide one's imperfections but to highlight one's features. Your big day is important to us. <br />We offer you with best makeup services, transforming your look and making sure that you looking stunning on your big day and throughout the wedding ceremonies. Artist here understands your requirements and provides you with the makeup that goes along well with your look, making sure that you slay  the day! <br />Services provided - Our services include makeup on venue for the bride and guests,  hairstyling, draping, men's grooming, nail extensions for all occasions.</p>`
     },
     {
       imageSrc: photography,
       imageAlt: "Photography Service",
       title: "Photography",
       btnHref: '/services/photography',
-      desc: "Photographs that communicate: Capture those life-cherishing moments with never seen before photography concepts. Make those traditional, candid, bride and groom moments even more rustic, authentic and vivid."
+      desc: `<p>Photographs that communicate: Capture those life-cherishing moments with never seen before photography concepts. Make those traditional, candid, bride and groom moments even more rustic, authentic and vivid.</p>`
     },
   ]
 
@@ -64,11 +62,7 @@ const Services = () => {
                 </div>
                 <div className={`lg:flex-grow md:w-1/2 ${index % 2 ? "lg:pl-24 md:pl-16" : "lg:pr-24 md:pr-16"} flex flex-col md:items-start md:text-left items-center text-center`}>
                   <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900">{hero.title}</h1>
-                  <p className="mb-8 leading-relaxed sm:text-xl text-sm" id='desc' >
-                    {hero.desc}{hero.title === "Makeup" ? <br /> : null}
-                    {hero.desc2}{hero.title === "Makeup" ? <br /> : null}
-                    {hero.desc3}
-                  </p>
+                  <div className="mb-8 leading-relaxed sm:text-xl text-sm" dangerouslySetInnerHTML={{ __html: hero.desc }} />
                   <div className="flex justify-center">
                     <Link rel="preload" href={hero.btnHref}>
                       <button className="learn-more">
